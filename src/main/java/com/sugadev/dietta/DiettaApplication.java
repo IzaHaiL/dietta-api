d727781f40da847cfe8ba5a8e29a0bd75ef1106d
Rizaldi Farhan <rizaldifarhan28@gmail.com>
2023-05-24 19:15:55 +0700

    Configure Dependency

---
+++ src/main/java/com/sugadev/dietta/DiettaApplication.java
@@ -0,0 +1,13 @@
+package com.sugadev.dietta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiettaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiettaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sugadev/dietta/DiettaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sugadev.dietta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiettaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
