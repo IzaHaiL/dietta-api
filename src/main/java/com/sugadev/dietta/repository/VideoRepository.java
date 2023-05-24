14cf17fe3a4cceb0f724c571a483e2dc681b3839
Rizaldi Farhan <rizaldifarhan28@gmail.com>
2023-05-24 22:04:16 +0700

    add video config

---
+++ src/main/java/com/sugadev/dietta/repository/VideoRepository.java
@@ -0,0 +1,7 @@
+package com.sugadev.dietta.repository;
+
+import com.sugadev.dietta.model.Video;
+import org.springframework.data.repository.CrudRepository;
+
+public interface VideoRepository  extends CrudRepository<Video, Integer> {
+}
