36331809396f7a8297440013c844582008177cb0
Rizaldi Farhan <rizaldifarhan28@gmail.com>
2023-06-16 11:23:00 +0700

    mengapus video

--- schedule-service/src/main/java/com/sugadev/scheduleservice/reposiotry/ScheduleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sugadev.scheduleservice.reposiotry;
-
-import com.sugadev.scheduleservice.model.Schedule;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ScheduleRepository extends JpaRepository <Schedule, Integer> {
-}
